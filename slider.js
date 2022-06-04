d018a7a80ce7c804469c8dfecc6ba3ffc04748b4
kyznecov <machoninviyacheslav@gmail.com>
2022-06-04 15:22:40 +0300

    commit slider.js

--- slider.js
+++ slider.js
@@ -1,23 +1,80 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    effect: "coverflow",
-    coverflowEffect: {
-      rotate: 50,
-      modifier: 1,
-      slideShadows: true,
-      depth: 100,
-    },
-    centeredSlides: true,
-    // spaceBetween: 30,
-    hashNavigation: {
-      watchState: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+"use strict";
+
+new Swiper(".cardSwiper", {
+  slidesPerView: 3,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    modifier: 1,
+    slideShadows: true,
+    depth: 100,
+  },
+  centeredSlides: true,
+  // spaceBetween: 30,
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  initialSlide: 3,
+});
+
+
+new Swiper(".teamSwiper", {
+  slidesPerView: 3,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    modifier: 1,
+    slideShadows: true,
+    depth: 100,
+  },
+  centeredSlides: true,
+  // spaceBetween: 30,
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  initialSlide: 1,
+});
+
+new Swiper(".rewiev", {
+  slidesPerView: 3,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    modifier: 1,
+    slideShadows: true,
+    depth: 100,
+  },
+  centeredSlides: true,
+  // spaceBetween: 30,
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  initialSlide: 1,
+});
